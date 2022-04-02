a3080e2313884dddd7dd90d429178c6024ad9d12
George007361 <gera.illarionov@mail.ru>
2022-04-02 06:45:56 +0300

    test 6

---
+++ lib/temperature_analyzer_serial/tests/test_temp_analyzer.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern "C" {
+#include "task.h"
+#include "temperature_analyzer.h"
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+TEST(TestSerialAlg, NoErr) {
+  delta_temperature_t dt;
+  char *fname = realpath("./examples/ex1.txt", NULL);
+  FILE *file = fopen(fname, "r");
+  EXPECT_EQ(find_max_temperature_delta_in_file(&dt, file), EXIT_SUCCESS);
+  EXPECT_EQ(dt.delta, 2001);
+  EXPECT_EQ(dt.index, 8);
+  fclose(file);
+}
